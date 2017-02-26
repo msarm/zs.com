75963532f3152d63d2a051541d51a6722da7a10a
msarm <msarm@users.noreply.github.com>
2017-02-26 10:58:23 -0600

    Initial Update

---
+++ wpscripts/nav_428tree-1.js
@@ -0,0 +1,66 @@
+var nav_428tree = WpNavBar.readTree({
+"childArray" : [
+{   'sTitle':'Why Zeetech',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'index.html',
+'sTarget':'_self'
+},
+{   'sTitle':'Academics',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'zeesys_html/Academics.html',
+'sTarget':'_self'
+},
+{   'sTitle':'MICE',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'zeesys_html/mice.html',
+'sTarget':'_self'
+},
+{   'sTitle':'Team',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'zeesys_html/team.html',
+'sTarget':'_self'
+},
+{   'sTitle':'Online Training',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'zeesys_html/OnlineTraining.html',
+'sTarget':'_self'
+},
+{   'sTitle':'Director\'s Message',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'zeesys_html/DirectorMessage.html',
+'sTarget':'_self'
+},
+{   'sTitle':'Alumni',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'zeesys_html/Alumni.html',
+'sTarget':'_self'
+},
+{   'sTitle':'Admin',
+'bIsExternal':true,
+'sUrl':'',
+'sTarget':'_self',"childArray" : [
+{   'sTitle':'Staff Timing',
+'bIsExternal':true,
+'sUrl':'http://goo.gl/forms/xZfo5BvaZ6',
+'sTarget':'_blank'
+},
+{   'sTitle':'ID Card',
+'bIsExternal':true,
+'sUrl':'http://goo.gl/forms/MpYLQjqA4x',
+'sTarget':'_blank'
+}]
+},
+{   'sTitle':'Contact Us',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'zeesys_html/ContactUs.html',
+'sTarget':'_self'
+}]
+});
---
+++ wpscripts/nav_428tree.js
@@ -0,0 +1,81 @@
+var nav_428tree = WpNavBar.readTree({
+"childArray" : [
+{   'sTitle':'Why Zeetech',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'index.html',
+'sTarget':'_self'
+},
+{   'sTitle':'Academics',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'zeesys_html/Academics.html',
+'sTarget':'_self'
+},
+{   'sTitle':'MICE',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'zeesys_html/mice.html',
+'sTarget':'_self'
+},
+{   'sTitle':'Team',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'zeesys_html/team.html',
+'sTarget':'_self'
+},
+{   'sTitle':'Online Training',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'zeesys_html/OnlineTraining.html',
+'sTarget':'_self'
+},
+{   'sTitle':'Director\'s Message',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'zeesys_html/DirectorMessage.html',
+'sTarget':'_self'
+},
+{   'sTitle':'Alumni',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'zeesys_html/Alumni.html',
+'sTarget':'_self'
+},
+{   'sTitle':'Admin',
+'bIsExternal':true,
+'sUrl':'',
+'sTarget':'_self',"childArray" : [
+{   'sTitle':'Staff Timing',
+'bIsExternal':true,
+'sUrl':'http://goo.gl/forms/xZfo5BvaZ6',
+'sTarget':'_blank'
+},
+{   'sTitle':'ID Card',
+'bIsExternal':true,
+'sUrl':'http://goo.gl/forms/MpYLQjqA4x',
+'sTarget':'_blank'
+},
+{   'sTitle':'Timing Report',
+'bIsExternal':true,
+'sUrl':'https://docs.google.com/spreadsheets/d/1-oHqjZPVdWCz3jxrJeyJKx0WD9gTWik6cL93bUR2Mjo/edit#gid=1591500156',
+'sTarget':'_blank'
+},
+{   'sTitle':'Net Accounts',
+'bIsExternal':true,
+'sUrl':'http://goo.gl/forms/vPzWbDQjOk',
+'sTarget':'_blank'
+},
+{   'sTitle':'Lab Accounts',
+'bIsExternal':true,
+'sUrl':'http://goo.gl/forms/hdWJjG7Iaa',
+'sTarget':'_self'
+}]
+},
+{   'sTitle':'Contact Us',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'zeesys_html/ContactUs.html',
+'sTarget':'_self'
+}]
+});
